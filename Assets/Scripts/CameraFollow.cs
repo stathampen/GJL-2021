a5e09bcd6b907d91f275eca4cd558541855bce1b
stathampen <33280323+stathampen@users.noreply.github.com>
2021-07-11 22:32:40 +0100

    player now moving along a track

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    [Range(0, 1)]
+    public float smoothTime;
+
+    public Vector3 offset = Vector3.zero;
+    private Vector3 velocity = Vector3.zero;
+
+    public Vector2 limits = new Vector2(5, 3);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            transform.localPosition = offset;
+        }
+
+
+        FollowTarget(target);
+    }
+
+    private void LateUpdate() {
+        Vector3 localPos = transform.localPosition;
+
+        //keep the camera a certain distance
+        transform.localPosition = new Vector3(Mathf.Clamp(localPos.x, -limits.y, limits.y), localPos.z);
+    }
+
+    public void FollowTarget(Transform target)
+    {
+        Vector3 localPos = transform.localPosition;
+        Vector3 targetLocalPos = target.transform.localPosition;
+        transform.localPosition = Vector3.SmoothDamp(localPos, new Vector3(targetLocalPos.x + offset.x, targetLocalPos.y + offset.y, localPos.z), ref velocity, smoothTime);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,25 +9,30 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]private Transform playerModel;
 
     public float shipSpeed = 18;
-    public float rotationSpeed = 10;
+    public float rotationSpeed = 340;
+    public float forwardSpeed = 6;
 
     public Transform aimTarget;
+
+    public bool joystick = true;
+
+    public CinemachineDollyCart dollyCart;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        setSpeed(forwardSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Mouse X");
-        float v = Input.GetAxis("Mouse Y");
+        float h = joystick ? Input.GetAxis("Horizontal") : Input.GetAxis("Mouse X");
+        float v = joystick ? Input.GetAxis("Vertical") : Input.GetAxis("Mouse Y");
 
         LocalMove(h, v, shipSpeed);
         RotationLook(h, v, rotationSpeed);
-        HorizontalLean(playerModel, h, 90, .1f);
+        HorizontalLean(playerModel, h, 80, .1f);
     }
 
     void LocalMove(float x, float y, float speed)
@@ -50,7 +56,7 @@ public class PlayerMovement : MonoBehaviour
     {
         aimTarget.parent.position = Vector3.zero;
         aimTarget.localPosition = new Vector3(h, v, 1);
-        transform.rotation =  Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed * Time.deltaTime);
     }
 
     void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)
@@ -59,12 +65,15 @@ public class PlayerMovement : MonoBehaviour
         target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
     }
 
-
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(aimTarget.position, .5f);
         Gizmos.DrawSphere(aimTarget.position, .15f);
+    }
+
+    private void setSpeed(float x)
+    {
+        dollyCart.m_Speed = x;
     }
 }
