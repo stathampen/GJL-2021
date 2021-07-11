f57776abe28b1446238b8676abe3639003723e3c
stathampen <33280323+stathampen@users.noreply.github.com>
2021-07-11 23:50:04 +0100

    brought in dot and input packages, looking to move over to thhe new input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using DG.Tweening;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -16,12 +17,14 @@ public class PlayerMovement : MonoBehaviour
 
     public bool joystick = true;
 
+    public bool isBoosting = false;
+
     public CinemachineDollyCart dollyCart;
 
     // Start is called before the first frame update
     void Start()
     {
-        setSpeed(forwardSpeed);
+        SetSpeed(forwardSpeed);
     }
 
     // Update is called once per frame
@@ -33,6 +36,13 @@ public class PlayerMovement : MonoBehaviour
         LocalMove(h, v, shipSpeed);
         RotationLook(h, v, rotationSpeed);
         HorizontalLean(playerModel, h, 80, .1f);
+
+        if (Input.GetButtonDown("Action"))
+            Boost(true);
+
+        if (Input.GetButtonUp("Action"))
+            Boost(false);
+
     }
 
     void LocalMove(float x, float y, float speed)
@@ -65,15 +75,33 @@ public class PlayerMovement : MonoBehaviour
         target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
     }
 
+    void Boost(bool state)
+    {
+        //need to tell camera to look fancy at this point?
+        if (state)
+        {
+            //whilst boosting do this
+            //put some fancy trails here
+        }
+        else
+        {
+            //do this instead
+        }
+
+        //if state is true times forward speed by 2, otherwise do not
+        float speed = state ? forwardSpeed * 2 : forwardSpeed;
+
+        DOVirtual.Float(dollyCart.m_Speed, speed, .15f, SetSpeed);
+    }
+    void SetSpeed(float x)
+    {
+        dollyCart.m_Speed = x;
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(aimTarget.position, .5f);
         Gizmos.DrawSphere(aimTarget.position, .15f);
-    }
-
-    private void setSpeed(float x)
-    {
-        dollyCart.m_Speed = x;
     }
 }
